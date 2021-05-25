9c904eea1829fc663b0eef637bc38e9a57bcbc13
NicholeKing <nichole.king94@hotmail.com>
2021-05-25 17:28:43 -0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import io from 'socket.io-client';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const imgStyle = {
+    width: '300px',
+    display: 'block'
+  }
+  const [socket] = useState(() => io(":8000"))
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const onChangeHandler = e => {
+    setInput(e.target.value);
+  }
+
+  const onSubmitHandler = e => {
+    e.preventDefault();
+    socket.emit('chat', input)
+  }
+
+  useEffect(() => {
+    console.log("Is this thing on???");
+    socket.on('post chat', msg => {
+      setMessages(prevmsgs => {return [msg, ...prevmsgs]})
+    })
+    return () => socket.disconnect(true);
+  }, [socket])
+  return (
+    <div className="App">
+      <form onSubmit={onSubmitHandler}>
+        <input type="text" name="msg" autoComplete="off" onChange={onChangeHandler}/>
+        <input type="submit" value="Submit"/>
+      </form>
+      {
+        messages.map((item, i) => {
+          return <img style={imgStyle} src={item} alt="picture" />
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
